98e5148b0be58234215eb5f519dc56901139001b
ajb85 <ajbrush85@gmail.com>
2019-02-13 16:44:15 -0500

    Text toggles strikethrough on click

--- src/App.js
+++ src/App.js
@@ -9,19 +9,33 @@ class App extends React.Component {
     this.state = {
       todo: [{ "Learn setState()": true }, { "Style my Todo List": true }]
     };
-  }
+  } // constructor
 
-  updateTodo = newItem => {
+  handleItemClick = e => {
+    let { todo } = this.state;
+    if (e.target.tagName === "P") {
+      //console.log(e.target.dataset.index);
+      const index = e.target.dataset.index;
+      const name = Object.keys(todo[index])[0];
+
+      todo[index][name] = !todo[index][name];
+    }
+
+    this.setState({ todo });
+  };
+
+  handleSubmit = newItem => {
     let { todo } = this.state;
     todo.push({ [newItem]: true });
 
     this.setState({ todo });
   };
+
   render() {
     return (
       <div className="container">
         <h1>Todo List: MVP</h1>
-        <CreateList todo={this.state.todo} />
+        <CreateList todo={this.state.todo} clicked={this.handleItemClick} />
         <CreateForm />
       </div>
     );
--- src/comps/List/CreateList.js
+++ src/comps/List/CreateList.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./list.css";
 
-// props: todo
+// props: todo, clicked
 export default props => {
   let fullList = props.todo.map((itemObj, i) => {
     let name = Object.keys(itemObj)[0];
     let className = "";
     if (!itemObj[name]) className = "strike";
     return (
-      <p key={i} className={className}>
+      <p key={i} onClick={props.clicked} className={className} data-index={i}>
         {name}
       </p>
     );
